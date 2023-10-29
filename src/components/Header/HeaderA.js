8e9eb65d55f2c437fdc30ed04905372c57f84808
John Doe <ibironkejohnsonade@gmail.com>
2023-10-29 13:08:20 -0700

    Initial Commit

---
+++ src/components/Header/HeaderA.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const HeaderA = () => {
+    return (
+        <View>
+            <Text>HeaderA</Text>
+        </View>
+    )
+}
+
+export default HeaderA
+
+const styles = StyleSheet.create({})
---
+++ src/screens/Main/Home/HomeScreen.js
@@ -0,0 +1,20 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import { COLORS } from '../../../constants'
+
+const HomeScreen = () => {
+    return (
+        <View style={styles.page}>
+            <Text>HomeScreen</Text>
+        </View>
+    )
+}
+
+export default HomeScreen
+
+const styles = StyleSheet.create({
+    page: {
+        flex: 1,
+        backgroundColor: COLORS.white,
+    },
+})
